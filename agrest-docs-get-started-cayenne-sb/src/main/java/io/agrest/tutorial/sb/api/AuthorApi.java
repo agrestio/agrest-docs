28c9c483d5f856844b55051d2453071775b6d357
Andrus Adamchik <andrus@objectstyle.com>
2022-05-14 11:22:25 +0200

    Getting Started ... endpoints

--- agrest-docs-get-started-cayenne-sb/src/main/java/io/agrest/tutorial/sb/api/AuthorApi.java
+++ agrest-docs-get-started-cayenne-sb/src/main/java/io/agrest/tutorial/sb/api/AuthorApi.java
@@ -11,7 +11,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
+// tag::base[]
 @Path("author")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,9 +21,17 @@ public class AuthorApi {
 
     @Context
     private Configuration config;
+// end::base[]
 
+// tag::post[]
     @POST
-    public DataResponse<Author> create(String data) {
-        return AgJaxrs.create(Author.class, config).syncAndSelect(data);
+    public DataResponse<Author> create(@Context UriInfo uri, String data) {
+        return AgJaxrs.create(Author.class, config) // <1>
+                .clientParams(uri.getQueryParameters()) // <2>
+                .syncAndSelect(data); // <3>
     }
+// end::post[]
+
+// tag::base[]
 }
+// end::base[]
--- agrest-docs-get-started-cayenne-sb/src/main/java/io/agrest/tutorial/sb/api/BookApi.java
+++ agrest-docs-get-started-cayenne-sb/src/main/java/io/agrest/tutorial/sb/api/BookApi.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
+// tag::base[]
 @Path("book")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,14 +23,18 @@ public class BookApi {
 
     @Context
     private Configuration config;
+// end::base[]
 
+// tag::post[]
     @POST
     public SimpleResponse create(String data) {
         return AgJaxrs.create(Book.class, config).sync(data);
     }
-
+// end::post[]
     @GET
     public DataResponse<Book> get(@Context UriInfo uriInfo) {
         return AgJaxrs.select(Book.class, config).clientParams(uriInfo.getQueryParameters()).get();
     }
+// tag::base[]
 }
+// end::base[]
