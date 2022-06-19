8ab80812d86fba29859895b7177039001b7d6a63
Andrus Adamchik <andrus@objectstyle.com>
2022-06-19 09:15:17 +0200

    Framework docs, in progress

--- agrest-docs-framework/src/main/java/io/agrest/docs/framework/AuthorApi.java
+++ agrest-docs-framework/src/main/java/io/agrest/docs/framework/AuthorApi.java
@@ -1,4 +1,32 @@
 package io.agrest.docs.framework;
 
+import io.agrest.DataResponse;
+import io.agrest.docs.framework.model.Author;
+import io.agrest.jaxrs3.AgJaxrs;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Configuration;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriInfo;
+
+// tag::all[]
+@Path("author")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AuthorApi {
+
+    @Context
+    private Configuration config; // <1>
+
+    @GET
+    public DataResponse<Author> get(@Context UriInfo uri) {
+        return AgJaxrs.select(Author.class, config) // <2>
+                .clientParams(uri.getQueryParameters())
+                .get();
+    }
 }
+// end::all[]
+
